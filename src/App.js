a6a557c24ba8c4a785a9198febd89372dc8fd519
Nitin Patel <nitinpatel278@gmail.com>
2019-06-10 17:41:28 +0530

    add material-ui and create navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 const App = () => {
-  return <p>Application code goes here</p>;
+  return <Navbar />;
 };
 
 export default App;
