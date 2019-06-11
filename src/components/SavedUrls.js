9d8a83b51f654697679a21eef6d14a86e7a6cc8f
Nitin Patel <nitinpatel278@gmail.com>
2019-06-11 14:52:22 +0530

    fix incorrect import formats in SavedUrls

--- src/components/SavedUrls.js
+++ src/components/SavedUrls.js
@@ -6,7 +6,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { Typography } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 import { getAllSavedUrls } from '../api';
 
 const useStyles = makeStyles(theme =>
